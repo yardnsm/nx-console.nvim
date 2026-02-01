e103acfa6d058584b94040589e4468f6d9032f95
Yarden Sod-Moriah <yardnsm@gmail.com>
2026-02-01 22:30:24 +0200

    feat: add yeet.nvim command runner
    Resolves #4.

--- lua/nx-console/runners/init.lua
+++ lua/nx-console/runners/init.lua
@@ -1,5 +1,6 @@
 local M = {}
 
 M.snacks = require("nx-console.runners.snacks")
+M.yeet = require("nx-console.runners.yeet")
 
 return M
---
+++ lua/nx-console/runners/yeet.lua
@@ -0,0 +1,19 @@
+local log = require("nx-console.log")
+
+---@param opts table | nil
+---@return nx_console.Config.CommandRunner
+local function create_yeet_runner(opts)
+  return function(cmd)
+    local has_yeet, yeet = pcall(require, "yeet")
+    if not has_yeet then
+      log.error("yeet.nvim is not installed")
+      return
+    end
+
+    local config = vim.tbl_deep_extend("force", {}, opts or {})
+
+    yeet.execute(cmd, config)
+  end
+end
+
+return create_yeet_runner
